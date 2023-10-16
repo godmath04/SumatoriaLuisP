e718f3ba4eb8cf80ee5fa8ffc6ba146831779b71
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2023-10-16 16:28:29 -0500

    Declaracion del for para ver que imprima lo correcto

--- Gauss.c
+++ Gauss.c
@@ -1,12 +1,24 @@
 #include<stdio.h>
 
-int main(void)
+int main()
 {
-    int numero, i, sumatoria = 0; 
+    int numero;
+    int i;
+    int sumatoria = 0; 
 
     printf(" SUMA DE GAUSS\n");
     printf("Escriba el numero para realizar la suma de Gauss:\n");
     scanf("%d", &numero);
-    printf("Su numero es: %d",numero);
+    printf("Su numero es: %d\n",numero);
+
+    for (int i = 1; i <= numero ; i++);
+    {
+        printf("Prueba de ver los numeros que se imprimen: %d\n", i);
+        //sumatoria = sumatoria + i;
+        //printf("\nLos numeros de la sumatoria son: %d",sumatoria);
+
+      
+    }
+    return 0; 
 
 }
