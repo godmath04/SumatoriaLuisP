bd86ae87941a8556bd6de3624720cea5e826826e
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2023-10-16 16:01:23 -0500

    Programa basico para comprar que si escribo un numero, este es impreso

---
+++ Gauss.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int numero, i, sumatoria = 0; 
+    printf("Conteo de numeros: ");
+    scanf("%d", &numero);
+    printf("Su numero es: %d",numero);
+
+}
