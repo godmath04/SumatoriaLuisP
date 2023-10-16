bd6347eb0ab35f3d64b6471f6c074892cc81bdd3
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2023-10-16 16:38:46 -0500

    Codigo Final de Gauss, comprobar la suma

--- Gauss.c
+++ Gauss.c
@@ -4,21 +4,19 @@ int main()
 {
     int numero;
     int i;
-    int sumatoria = 0; 
+    int sumatoria=0; 
 
     printf(" SUMA DE GAUSS\n");
     printf("Escriba el numero para realizar la suma de Gauss:\n");
     scanf("%d", &numero);
     printf("Su numero es: %d\n",numero);
 
-    for (int i = 1; i <= numero ; i++);
+    for (i = 1; i <= numero; i++);
     {
-        printf("Prueba de ver los numeros que se imprimen: %d\n", i);
-        //sumatoria = sumatoria + i;
-        //printf("\nLos numeros de la sumatoria son: %d",sumatoria);
-
-      
+        sumatoria = sumatoria+i;
     }
+
+    printf("El resultado de la suma de Gauss es: %d",sumatoria);
     return 0; 
 
 }
